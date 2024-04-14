116eaabef5d18dd732c7f2dfaada5e40efe482cc
smarkandu <s_markan@hotmail.com>
2024-04-14 04:15:10 -0400

    Pass text_padded to model

--- recipes/LJSpeech/TTS/tacotron2/train.py
+++ recipes/LJSpeech/TTS/tacotron2/train.py
@@ -58,10 +58,12 @@ class Tacotron2Brain(sb.Brain):
         effective_batch = self.batch_to_device(batch)
         inputs, y, num_items, _, _ = effective_batch
 
-        _, input_lengths, _, _, _ = inputs
+        text_padded, input_lengths, _, _, _ = inputs
+        # text_padded, input_lengths, mel_padded, max_len, output_lengths
+        print("text_padded: " + type(text_padded))
 
         max_input_length = input_lengths.max().item()
-        return self.modules.model(inputs)
+        return self.modules.model(text_padded)
 
     def on_fit_batch_end(self, batch, outputs, loss, should_step):
         """At the end of the optimizer step, apply noam annealing."""
