3677f8a4645dd194c1f7adb4af6ef4d3654dfe80
smarkandu <s_markan@hotmail.com>
2024-04-14 04:22:44 -0400

    added print statement

--- recipes/LJSpeech/TTS/tacotron2/train.py
+++ recipes/LJSpeech/TTS/tacotron2/train.py
@@ -60,7 +60,9 @@ class Tacotron2Brain(sb.Brain):
 
         text_padded, input_lengths, _, _, _ = inputs
         # text_padded, input_lengths, mel_padded, max_len, output_lengths
-        print("text_padded: " + str(type(text_padded)))
+        f = open("steve.txt", "a")
+        f.write("text_padded: " + str(type(text_padded)))
+        f.close()
 
         max_input_length = input_lengths.max().item()
         return self.modules.model(text_padded)
